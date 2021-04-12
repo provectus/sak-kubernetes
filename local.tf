4b692d85708922af3070f4903277ede381293be9
akastav <switch@bk.ru>
2021-04-12 12:38:34 +0300

    fix variables

--- local.tf
+++ local.tf
@@ -1,8 +1,8 @@
 locals {
   common = values({
-    for index, customer in var.customers :
-    customer => {
-      name_prefix                              = "on-demand-${customer}-${index}"
+    for index, az in var.availability_zones :
+    az => {
+      name_prefix                              = "on-demand-common-${index}"
       instance_type                            = var.on_demand_common_instance_type
       override_instance_types                  = var.on_demand_common_override_instance_types
       asg_max_size                             = var.on_demand_common_max_cluster_size
@@ -36,21 +36,22 @@ locals {
     }
   })
 
-  cpu = values({
-    "cpu" = {
-      name_prefix                              = "on-demand-cpu-"
-      instance_type                            = var.on_demand_cpu_instance_type
-      override_instance_types                  = var.on_demand_cpu_override_instance_types
-      asg_max_size                             = var.on_demand_cpu_max_cluster_size
-      asg_min_size                             = var.on_demand_cpu_min_cluster_size
-      asg_desired_capacity                     = var.on_demand_cpu_desired_capacity
-      asg_recreate_on_change                   = var.on_demand_cpu_asg_recreate_on_change
-      on_demand_allocation_strategy            = var.on_demand_cpu_allocation_strategy
-      on_demand_base_capacity                  = var.on_demand_cpu_base_capacity
-      on_demand_percentage_above_base_capacity = var.on_demand_cpu_percentage_above_base_capacity
+  customers = values({
+    for index, customer in var.customers :
+    customer => {
+      name_prefix                              = "on-demand-${customer}-${index}"
+      instance_type                            = var.on_demand_customer_instance_type
+      override_instance_types                  = var.on_demand_customer_override_instance_types
+      asg_max_size                             = var.on_demand_customer_max_cluster_size
+      asg_min_size                             = var.on_demand_customer_min_cluster_size
+      asg_desired_capacity                     = var.on_demand_customer_desired_capacity
+      asg_recreate_on_change                   = var.on_demand_customer_asg_recreate_on_change
+      on_demand_allocation_strategy            = var.on_demand_customer_allocation_strategy
+      on_demand_base_capacity                  = var.on_demand_customer_base_capacity
+      on_demand_percentage_above_base_capacity = var.on_demand_customer_percentage_above_base_capacity
       autoscaling_enabled                      = false
       subnets                                  = var.subnets
-      kubelet_extra_args                       = join(" ", ["--node-labels=node.kubernetes.io/lifecycle=normal,node-type=cpu", "--register-with-taints=node-type=cpu:NoSchedule"])
+      kubelet_extra_args                       = join(" ", ["--node-labels=node.kubernetes.io/lifecycle=normal,node-type=${customer}", "--register-with-taints=node-type=${customer}:NoSchedule"])
       suspended_processes                      = ["AZRebalance"]
       tags = [
         {
@@ -66,69 +67,12 @@ locals {
         {
           "key"                 = "k8s.io/cluster-autoscaler/node-template/label/node-type"
           "propagate_at_launch" = "false"
-          "value"               = "cpu"
+          "value"               = "${customer}"
         },
         {
           "key"                 = "k8s.io/cluster-autoscaler/node-template/taint/node-type"
           "propagate_at_launch" = "false"
-          "value"               = "cpu:NoSchedule"
-        }
-      ]
-    }
-  })
-
-  gpu = values({
-    "gpu" = {
-      name_prefix                              = "on-demand-gpu-"
-      instance_type                            = var.on_demand_gpu_instance_type
-      override_instance_types                  = var.on_demand_gpu_override_instance_types
-      asg_max_size                             = var.on_demand_gpu_max_cluster_size
-      asg_min_size                             = var.on_demand_gpu_min_cluster_size
-      asg_desired_capacity                     = var.on_demand_gpu_desired_capacity
-      asg_recreate_on_change                   = var.on_demand_gpu_asg_recreate_on_change
-      on_demand_allocation_strategy            = var.on_demand_gpu_allocation_strategy
-      on_demand_base_capacity                  = var.on_demand_gpu_base_capacity
-      on_demand_percentage_above_base_capacity = var.on_demand_gpu_percentage_above_base_capacity
-      ami_id                                   = data.aws_ami.eks_gpu_worker.id
-      autoscaling_enabled                      = false
-      subnets                                  = var.subnets
-      kubelet_extra_args                       = join(" ", ["--node-labels=node.kubernetes.io/lifecycle=normal,node-type=gpu,nvidia.com/gpu=gpu", "--register-with-taints=node-type=gpu:NoSchedule,nvidia.com/gpu=gpu:NoSchedule"])
-      suspended_processes                      = ["AZRebalance"]
-      tags = [
-        {
-          "key"                 = "k8s.io/cluster-autoscaler/enabled"
-          "propagate_at_launch" = "false"
-          "value"               = "true"
-        },
-        {
-          "key"                 = "k8s.io/cluster-autoscaler/${var.cluster_name}"
-          "propagate_at_launch" = "false"
-          "value"               = "owned"
-        },
-        {
-          "key"                 = "k8s.io/cluster-autoscaler/node-template/label/node-type"
-          "propagate_at_launch" = "false"
-          "value"               = "gpu"
-        },
-        {
-          "key"                 = "k8s.io/cluster-autoscaler/node-template/label/nvidia.com/gpu"
-          "propagate_at_launch" = "false"
-          "value"               = "gpu"
-        },
-        {
-          "key"                 = "k8s.io/cluster-autoscaler/node-template/taint/node-type"
-          "propagate_at_launch" = "false"
-          "value"               = "gpu:NoSchedule"
-        },
-        {
-          "key"                 = "k8s.io/cluster-autoscaler/node-template/taint/nvidia.com/gpu"
-          "propagate_at_launch" = "false"
-          "value"               = "gpu:NoSchedule"
-        },
-        {
-          "key"                 = "k8s.io/cluster-autoscaler/node-template/resources/nvidia.com/gpu"
-          "propagate_at_launch" = "false"
-          "value"               = var.on_demand_gpu_resource_count
+          "value"               = "${customer}:NoSchedule"
         }
       ]
     }
