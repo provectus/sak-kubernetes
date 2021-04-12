6845f0b881238da80c671c55b0812553ca1c7b7b
akastav <switch@bk.ru>
2021-04-12 12:42:55 +0300

    fix variables

--- variables.tf
+++ variables.tf
@@ -171,51 +171,51 @@ variable "on_demand_gpu_asg_recreate_on_change" {
 }
 
 # On-demand CPU instance
-variable "on_demand_cpu_max_cluster_size" {
+variable "on_demand_customer_max_cluster_size" {
   type        = string
   description = "Max number of on demand instances in EKS autoscaling group"
   default     = "2"
 }
 
-variable "on_demand_cpu_min_cluster_size" {
+variable "on_demand_customer_min_cluster_size" {
   type        = string
   description = "Min number of on demand instances in EKS autoscaling group"
   default     = "0"
 }
 
-variable "on_demand_cpu_desired_capacity" {
+variable "on_demand_customer_desired_capacity" {
   type        = string
   description = "Desired number of on_demand instances in EKS autoscaling group"
   default     = "0"
 }
 
-variable "on_demand_cpu_instance_type" {
+variable "on_demand_customer_instance_type" {
   description = "EC2 on_demand Instance type"
   default     = "c5.xlarge"
 }
 
-variable "on_demand_cpu_override_instance_types" {
+variable "on_demand_customer_override_instance_types" {
   description = "EC2 on_demand Instance type"
   default     = ["c5.xlarge", "c5.2xlarge", "c5n.xlarge"]
 }
 
-variable "on_demand_cpu_allocation_strategy" {
+variable "on_demand_customer_allocation_strategy" {
   description = "Strategy to use when launching on-demand instances. Valid values: prioritized"
   default     = "prioritized"
 }
 
-variable "on_demand_cpu_base_capacity" {
+variable "on_demand_customer_base_capacity" {
   description = "Absolute minimum amount of desired capacity that must be fulfilled by on-demand instances"
   default     = "0"
 
 }
 
-variable "on_demand_cpu_percentage_above_base_capacity" {
+variable "on_demand_customer_percentage_above_base_capacity" {
   description = "Percentage split between on-demand and Spot instances above the base on-demand capacity"
   default     = "0"
 }
 
-variable "on_demand_cpu_asg_recreate_on_change" {
+variable "on_demand_customer_asg_recreate_on_change" {
   description = "Recreate the autoscaling group when the Launch Template or Launch Configuration change."
   default     = "false"
 }
