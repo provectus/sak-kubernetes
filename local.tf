958d5c306cc027d85e0d76fc64fb160854bd75f5
akastav <switch@bk.ru>
2021-06-08 09:21:02 +0300

    docker-config-json remove quotes

--- local.tf
+++ local.tf
@@ -1,9 +1,10 @@
 locals {
   registry = "https://registry.${var.domains[0]}"
 
-  docker_config_json = jsonencode({
+  docker_config_json = jsonencode(
+  {
     "registry-mirrors" = ["${local.registry}"]
-    })
+  })
 
   common = values({
     for index, az in var.availability_zones :
