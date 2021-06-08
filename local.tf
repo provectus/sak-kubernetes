06e08d3351558b89c0b41ee0893c0d5924c4b02f
akastav <switch@bk.ru>
2021-06-08 08:39:43 +0300

    move docker-config-json to local

--- local.tf
+++ local.tf
@@ -1,6 +1,8 @@
 locals {
   registry = "https://registry.${var.domains[0]}"
 
+  docker_config_json = ${jsonencode({"registry-mirrors"="["${local.registry}"]"})}
+
   common = values({
     for index, az in var.availability_zones :
     az => {
