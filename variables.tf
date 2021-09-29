ddbe5a96309fad93f0fa9eed1701a21fb016bbd1
Dmitriy Miroshnik <dmiroshnik@provectus.com>
2021-09-29 12:31:34 +0300

    add type for ondemand_ variables

--- variables.tf
+++ variables.tf
@@ -103,155 +103,174 @@ variable "user_arns" {
 # }
 
 # On-demand instance
-# variable "on_demand_common_max_cluster_size" {
-#   type        = string
-#   description = "Max number of on demand instances in EKS autoscaling group"
-#   default     = "2"
-# }
-#
-# variable "on_demand_common_min_cluster_size" {
-#   type        = string
-#   description = "Min number of on demand instances in EKS autoscaling group"
-#   default     = "1"
-# }
-#
-# variable "on_demand_common_desired_capacity" {
-#   type        = string
-#   description = "Desired number of on_demand instances in EKS autoscaling group"
-#   default     = "1"
-# }
-#
-# variable "on_demand_common_instance_type" {
-#   description = "EC2 on_demand Instance type"
-#   default     = "m5.large"
-# }
-#
-# variable "on_demand_common_override_instance_types" {
-#   description = "EC2 on_demand override instance types"
-#   default     = ["m5.large", "m5.xlarge", "m5.2xlarge"]
-# }
-#
-# variable "on_demand_common_allocation_strategy" {
-#   description = "Strategy to use when launching on-demand instances. Valid values: prioritized"
-#   default     = "prioritized"
-# }
-#
-# variable "on_demand_common_base_capacity" {
-#   description = "Absolute minimum amount of desired capacity that must be fulfilled by on-demand instances"
-#   default     = "0"
-#
-# }
-#
-# variable "on_demand_common_percentage_above_base_capacity" {
-#   description = "Percentage split between on-demand and Spot instances above the base on-demand capacity"
-#   default     = "100"
-# }
-#
-# variable "on_demand_common_asg_recreate_on_change" {
-#   description = "Recreate the autoscaling group when the Launch Template or Launch Configuration change."
-#   default     = "true"
-# }
-#
-# # On-demand GPU instance
-# variable "on_demand_gpu_max_cluster_size" {
-#   type        = string
-#   description = "Max number of on demand instances in EKS autoscaling group"
-#   default     = "2"
-# }
-#
-# variable "on_demand_gpu_min_cluster_size" {
-#   type        = string
-#   description = "Min number of on demand instances in EKS autoscaling group"
-#   default     = "0"
-# }
-#
-# variable "on_demand_gpu_desired_capacity" {
-#   type        = string
-#   description = "Desired number of on_demand instances in EKS autoscaling group"
-#   default     = "0"
-# }
-#
-# variable "on_demand_gpu_instance_type" {
-#   description = "EC2 on_demand Instance type"
-#   default     = "p2.xlarge"
-# }
-#
-# variable "on_demand_gpu_override_instance_types" {
-#   description = "EC2 on_demand Instance types for overriding"
-#   default     = ["g4dn.xlarge"]
-# }
-#
-# variable "on_demand_gpu_resource_count" {
-#   description = "A number of GPUs resopurces for the instance type"
-#   default     = 1
-# }
-#
-# variable "on_demand_gpu_allocation_strategy" {
-#   description = "Strategy to use when launching on-demand instances. Valid values: prioritized"
-#   default     = "prioritized"
-# }
-#
-# variable "on_demand_gpu_base_capacity" {
-#   description = "Absolute minimum amount of desired capacity that must be fulfilled by on-demand instances"
-#   default     = "0"
-# }
-#
-# variable "on_demand_gpu_percentage_above_base_capacity" {
-#   description = "Percentage split between on-demand and Spot instances above the base on-demand capacity"
-#   default     = "100"
-# }
-#
-# variable "on_demand_gpu_asg_recreate_on_change" {
-#   description = "Recreate the autoscaling group when the Launch Template or Launch Configuration change."
-#   default     = "true"
-# }
-#
-# # On-demand CPU instance
-# variable "on_demand_cpu_max_cluster_size" {
-#   type        = string
-#   description = "Max number of on demand instances in EKS autoscaling group"
-#   default     = "2"
-# }
-#
-# variable "on_demand_cpu_min_cluster_size" {
-#   type        = string
-#   description = "Min number of on demand instances in EKS autoscaling group"
-#   default     = "0"
-# }
-#
-# variable "on_demand_cpu_desired_capacity" {
-#   type        = string
-#   description = "Desired number of on_demand instances in EKS autoscaling group"
-#   default     = "0"
-# }
-#
-# variable "on_demand_cpu_instance_type" {
-#   description = "EC2 on_demand Instance type"
-#   default     = "c5.xlarge"
-# }
-#
-# variable "on_demand_cpu_override_instance_types" {
-#   description = "EC2 on_demand Instance type"
-#   default     = ["c5.xlarge", "c5.2xlarge", "c5n.xlarge"]
-# }
-#
-# variable "on_demand_cpu_allocation_strategy" {
-#   description = "Strategy to use when launching on-demand instances. Valid values: prioritized"
-#   default     = "prioritized"
-# }
-#
-# variable "on_demand_cpu_base_capacity" {
-#   description = "Absolute minimum amount of desired capacity that must be fulfilled by on-demand instances"
-#   default     = "0"
-#
-# }
-#
-# variable "on_demand_cpu_percentage_above_base_capacity" {
-#   description = "Percentage split between on-demand and Spot instances above the base on-demand capacity"
-#   default     = "100"
-# }
-#
-# variable "on_demand_cpu_asg_recreate_on_change" {
-#   description = "Recreate the autoscaling group when the Launch Template or Launch Configuration change."
-#   default     = "true"
-# }
+variable "on_demand_common_max_cluster_size" {
+  type        = string
+  description = "Max number of on demand instances in EKS autoscaling group"
+  default     = "2"
+}
+
+variable "on_demand_common_min_cluster_size" {
+  type        = string
+  description = "Min number of on demand instances in EKS autoscaling group"
+  default     = "1"
+}
+
+variable "on_demand_common_desired_capacity" {
+  type        = string
+  description = "Desired number of on_demand instances in EKS autoscaling group"
+  default     = "1"
+}
+
+variable "on_demand_common_instance_type" {
+  type        = string
+  description = "EC2 on_demand Instance type"
+  default     = "m5.large"
+}
+
+variable "on_demand_common_override_instance_types" {
+  type        = list(string)
+  description = "EC2 on_demand override instance types"
+  default     = ["m5.large", "m5.xlarge", "m5.2xlarge"]
+}
+
+variable "on_demand_common_allocation_strategy" {
+  type        = string
+  description = "Strategy to use when launching on-demand instances. Valid values: prioritized"
+  default     = "prioritized"
+}
+
+variable "on_demand_common_base_capacity" {
+  type        = string
+  description = "Absolute minimum amount of desired capacity that must be fulfilled by on-demand instances"
+  default     = "0"
+
+}
+
+variable "on_demand_common_percentage_above_base_capacity" {
+  type        = string
+  description = "Percentage split between on-demand and Spot instances above the base on-demand capacity"
+  default     = "100"
+}
+
+variable "on_demand_common_asg_recreate_on_change" {
+  type        = string
+  description = "Recreate the autoscaling group when the Launch Template or Launch Configuration change."
+  default     = "true"
+}
+
+# On-demand GPU instance
+variable "on_demand_gpu_max_cluster_size" {
+  type        = string
+  description = "Max number of on demand instances in EKS autoscaling group"
+  default     = "2"
+}
+
+variable "on_demand_gpu_min_cluster_size" {
+  type        = string
+  description = "Min number of on demand instances in EKS autoscaling group"
+  default     = "0"
+}
+
+variable "on_demand_gpu_desired_capacity" {
+  type        = string
+  description = "Desired number of on_demand instances in EKS autoscaling group"
+  default     = "0"
+}
+
+variable "on_demand_gpu_instance_type" {
+  type        = string
+  description = "EC2 on_demand Instance type"
+  default     = "p2.xlarge"
+}
+
+variable "on_demand_gpu_override_instance_types" {
+  type        = list(string)
+  description = "EC2 on_demand Instance types for overriding"
+  default     = ["g4dn.xlarge"]
+}
+
+variable "on_demand_gpu_resource_count" {
+  type        = number
+  description = "A number of GPUs resopurces for the instance type"
+  default     = 1
+}
+
+variable "on_demand_gpu_allocation_strategy" {
+  type        = string
+  description = "Strategy to use when launching on-demand instances. Valid values: prioritized"
+  default     = "prioritized"
+}
+
+variable "on_demand_gpu_base_capacity" {
+  type        = string
+  description = "Absolute minimum amount of desired capacity that must be fulfilled by on-demand instances"
+  default     = "0"
+}
+
+variable "on_demand_gpu_percentage_above_base_capacity" {
+  type        = string
+  description = "Percentage split between on-demand and Spot instances above the base on-demand capacity"
+  default     = "100"
+}
+
+variable "on_demand_gpu_asg_recreate_on_change" {
+  type        = string
+  description = "Recreate the autoscaling group when the Launch Template or Launch Configuration change."
+  default     = "true"
+}
+
+# On-demand CPU instance
+variable "on_demand_cpu_max_cluster_size" {
+  type        = string
+  description = "Max number of on demand instances in EKS autoscaling group"
+  default     = "2"
+}
+
+variable "on_demand_cpu_min_cluster_size" {
+  type        = string
+  description = "Min number of on demand instances in EKS autoscaling group"
+  default     = "0"
+}
+
+variable "on_demand_cpu_desired_capacity" {
+  type        = string
+  description = "Desired number of on_demand instances in EKS autoscaling group"
+  default     = "0"
+}
+
+variable "on_demand_cpu_instance_type" {
+  type        = string
+  description = "EC2 on_demand Instance type"
+  default     = "c5.xlarge"
+}
+
+variable "on_demand_cpu_override_instance_types" {
+  type        = list(string)
+  description = "EC2 on_demand Instance type"
+  default     = ["c5.xlarge", "c5.2xlarge", "c5n.xlarge"]
+}
+
+variable "on_demand_cpu_allocation_strategy" {
+  type        = string
+  description = "Strategy to use when launching on-demand instances. Valid values: prioritized"
+  default     = "prioritized"
+}
+
+variable "on_demand_cpu_base_capacity" {
+  type        = string
+  description = "Absolute minimum amount of desired capacity that must be fulfilled by on-demand instances"
+  default     = "0"
+
+}
+
+variable "on_demand_cpu_percentage_above_base_capacity" {
+  type        = string
+  description = "Percentage split between on-demand and Spot instances above the base on-demand capacity"
+  default     = "100"
+}
+
+variable "on_demand_cpu_asg_recreate_on_change" {
+  type        = string
+  description = "Recreate the autoscaling group when the Launch Template or Launch Configuration change."
+  default     = "true"
+}
